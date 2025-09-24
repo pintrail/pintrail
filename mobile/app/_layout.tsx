6201d8e4feb70a40df908ba13a3de48add78a16b
yongyetan1209 <yongyetan1209@gmail.com>
2025-09-24 14:40:19 -0400

    add barebone expo app

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+      <StatusBar style="auto" />
+    </>
+  );
+}
