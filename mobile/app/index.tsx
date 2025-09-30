eeac04149d3721578bb4f43b0648ad1900a526cb
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2025-09-30 00:00:10 -0400

    playing around with Marker

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import MapView, { Region } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 export default function HomeScreen() {
 
@@ -17,11 +17,25 @@ export default function HomeScreen() {
     setState(region);
   };
 
+
   const [state, setState] = useState(getInitialRegion());
+  const [open, setOpen] = useState(false)
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} region={state} onRegionChange={onRegionChange} />
+      <MapView style={styles.map} region={state} onRegionChange={onRegionChange} onPress={() => setOpen(false)}> 
+        <Marker coordinate={{ latitude: 42.39674910914364, longitude: -72.52415362726558 }} title='North B' pinColor='red' description='This is where Felix lives' onPress={ (e) => { e.stopPropagation(); setOpen(true); } }/>
+      </MapView>
+
+      {open && (
+        <View style={styles.markerExpanded}>
+          <Text>North B</Text>
+          <Pressable style={styles.markerExitButton} onPress={() => setOpen(false)}>
+            <Text>X</Text>
+          </Pressable>
+        </View>
+      )}
+      
     </View>
   );
 }
@@ -34,4 +48,25 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  markerExpanded: {
+    position:'absolute',
+    height: '50%',
+    width: '100%',
+    backgroundColor: 'white',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20
+  },
+  markerExitButton: {
+    backgroundColor: 'grey',
+    padding: 5,
+    alignContent: 'center',
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 20
+  }
 });
